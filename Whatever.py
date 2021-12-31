ee347689ab252fdc1929108ff39dd262219f1a3b
Snowey1110 <55231387+Snowey1110@users.noreply.github.com>
2021-12-31 02:15:02 -0800

    Updated Whatever.py making it better c: also added list of fireworks for people to paste their firework commands

--- Whatever.py
+++ Whatever.py
@@ -1,9 +1,39 @@
-with open('fireworks/data/fireworks/functions/shows/2021_22_show_1.mcfunction', 'w') as file:
-    x = 0
-    y = 100
-    z = 0
-    for line in range(1, 11):
-        file.write(f'execute if score showTime ShowTimer1 matches {line} run summon firework_rocket {x} {y} {z} '
-                   f'{{LifeTime:15,FireworksItem:{{id:"firework_rocket",Count:1,tag:{{Fireworks:{{Explosions:'
-                   f'[{{Type:4,Flicker:0,Trail:0,Colors:[I;5554263,5407270,9576437,16607458],FadeColors:[I;14206582,1322348,9863818,16710712]}}],Flight:2}}}}}}}}\n')
+import random
 
+COORDS = (0, 20, 0)
+X_SPREAD = 20
+Y_SPREAD = 5
+Z_SPREAD = 20
+REPEAT = 20
+LIFETIME_SPREAD = 15
+
+fireworkList = []
+
+with open('List Of Fireworks', 'r') as fireworks:
+
+    for line in fireworks:
+        line = f'\t{line[0:-1]}'
+        lineArray = line.split("id:firework_rocket")
+        if len(lineArray) == 1:
+            lineArray = line.split('id:"firework_rocket"')
+        if len(lineArray) == 1:
+            lineArray = line.split("id:'firework_rocket'")
+        if len(lineArray) != 2:
+            continue
+        line = lineArray[1]
+        fireworkList.append(f'{{id:firework_rocket{line}')
+
+
+def randomLocation(xSpread = X_SPREAD, ySpread = Y_SPREAD, zSpread = Z_SPREAD, r = REPEAT,centerCoord = COORDS, lifetimeSpread = LIFETIME_SPREAD):
+    with open('fireworks/data/fireworks/functions/shows/2021_22_show_1.mcfunction', 'w') as file:
+        for line in range(1, r+1):
+            x = centerCoord[0] + random.randint(-xSpread, xSpread)
+            y = centerCoord[1] + random.randint(-ySpread, ySpread)
+            z = centerCoord[2] + random.randint(-zSpread, zSpread)
+            lifetime = random.randint(0, lifetimeSpread + 1)
+            fireworkType = random.choice(fireworkList)
+            file.write(f'execute if score showTime ShowTimer1 matches {line} run summon firework_rocket {x} {y} {z} '
+                       f'{{LifeTime:{lifetime},FireworksItem:{fireworkType}}}\n')
+
+
+randomLocation()
